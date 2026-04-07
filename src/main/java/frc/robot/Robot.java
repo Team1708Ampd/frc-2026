62674fe0681a1b624d225b901d86e4eae4ea5086
Team1708Ampd <first.team1708@gmail.com>
2026-04-07 19:40:43 -0400

    zeroing encoder on robot init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -58,6 +58,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
         LimelightHelpers.SetIMUMode("limelight", 4);
+        Robot.shooterSub.zeroHoodEncoder();
     }
 
     @Override
